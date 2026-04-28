90ca8cb3686b35dfc59f14d846e41b4b2e5c9dc7
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-27 21:12:56 -0300

    feat/contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from "react";
+
+function Contact(): JSX.Element {
+    return (
+        <div>
+            <h1>Contacto</h1>
+            <p>Información de contacto.</p>
+        </div>
+    );
+}
+
+export default Contact;
