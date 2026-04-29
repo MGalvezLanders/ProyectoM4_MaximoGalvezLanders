2f31e1dfa30beb88d723d94ebcec8e63abd59f5d
MGalvezLanders <galvezlandersmaximo@gmail.com>
2026-04-29 16:44:01 -0300

    feat/ error validation and authentication with firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Exportamos auth para usarlo en los flujos de autenticación
+export const auth = getAuth(app);
+
+
+ 
